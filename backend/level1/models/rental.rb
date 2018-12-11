38c802aaeaa60cc7637614029179ae9b626089b0
Arno DUBOIS <arno.du@orange.fr>
2018-12-11 12:12:54 +0100

    Last bit ?

--- backend/level1/models/rental.rb
+++ backend/level1/models/rental.rb
@@ -4,11 +4,13 @@ class Rental
   def initialize(id, car, start_date, end_date, distance)
     @id = id
     @car = car
-    start_date = start_date
-    end_date = end_date
     @distance = distance.to_i
 
-    @days = (Date.parse(end_date) - Date.parse(start_date) + 1).to_i
+    @days = days_difference(start_date, end_date)
+  end
+
+  def days_difference(start_date, end_date)
+    (Date.parse(start_date)..Date.parse(end_date)).count
   end
 
   def time_price
